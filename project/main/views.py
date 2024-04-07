b09d899ef59780e292cef1e73c02da36eb286cb7
Persona99 <vadim.zhelnin@bk.ru>
2024-04-07 18:10:39 +0500

    update get clients view

--- project/main/views.py
+++ project/main/views.py
@@ -8,30 +8,30 @@ from django.db.models import F, Q
 
 class Clients(APIView):
     def get(self, request: Request) -> Response:
-        #     try:
-        limit = int(request.query_params.get('limit', 10))
-        page = int(request.query_params.get('page', 1))
-        sortBy = request.query_params.get('sortBy', 'createAt')
-        sortDir = request.query_params.get('sortDir', 'asc')
-        search = request.query_params.get('search', '')
-        if sortDir == 'desc':
-            instances = Client.objects.all().filter(deleteAt=None).filter(
-                Q(name__contains=search) | Q(surname__contains=search) | Q(patronymic__contains=search)).order_by(
-                    F(sortBy).desc())[(page-1)*limit:page*limit]
-        else:
-            instances = Client.objects.all().filter(deleteAt=None).filter(
-                Q(name__contains=search) | Q(surname__contains=search) | Q(patronymic__contains=search)).order_by(
-                    F(sortBy).asc())[(page-1)*limit:page*limit]
-        serializer = GetClientsResponse(instances, many=True)
-        data = {
-            'limit': limit,
-            'page': page,
-            'total': Client.objects.filter(deleteAt=None).count(),
-            'data': serializer.data
-        }
-        return Response(data=data, status=200)
-        # except:
-        #     return Response(data={"status": 500, "code": "INTERNAL_SERVER_ERROR"}, status=500)
+        try:
+            limit = int(request.query_params.get('limit', 10))
+            page = int(request.query_params.get('page', 1))
+            sortBy = request.query_params.get('sortBy', 'createAt')
+            sortDir = request.query_params.get('sortDir', 'asc')
+            search = request.query_params.get('search', '')
+            if sortDir == 'desc':
+                instances = Client.objects.all().filter(deleteAt=None).filter(
+                    Q(name__contains=search) | Q(surname__contains=search) | Q(patronymic__contains=search)).order_by(
+                        F(sortBy).desc())[(page-1)*limit:page*limit]
+            else:
+                instances = Client.objects.all().filter(deleteAt=None).filter(
+                    Q(name__contains=search) | Q(surname__contains=search) | Q(patronymic__contains=search)).order_by(
+                        F(sortBy).asc())[(page-1)*limit:page*limit]
+            serializer = GetClientsResponse(instances, many=True)
+            data = {
+                'limit': limit,
+                'page': page,
+                'total': Client.objects.filter(deleteAt=None).count(),
+                'data': serializer.data
+            }
+            return Response(data=data, status=200)
+        except:
+            return Response(data={"status": 500, "code": "INTERNAL_SERVER_ERROR"}, status=500)
 
     def post(self, request: Request) -> Response:
         try:
